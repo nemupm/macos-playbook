3efae2272d13faa8547cc8d5c36b48bd1805ff90
nemupm <nemupm@gmail.com>
2019-09-09 16:53:27 +0900

    rm

--- roles/environment/files/Documents/script/backup_icloud.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-PATH=/usr/local/bin:/usr/bin:/bin
-
-mkdir -p $HOME/Documents/resource/icloud/
-rsync -a $HOME/Library/Mobile\ Documents/com~apple~CloudDocs/ $HOME/Documents/resource/icloud/backup --exclude ".git"
-cd $HOME/Documents/resource/icloud/backup
-git init && git add -A && git commit -m "update"
